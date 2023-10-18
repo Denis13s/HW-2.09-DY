08bf7d63bbd38402fa9485ecd521213559d155fd
Denis13s <141165300+Denis13s@users.noreply.github.com>
2023-10-18 15:49:29 +0200

    Initial Commit

---
+++ HW-2.09-DY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-2.09-DY
+//
+//  Created by Denis Yarets on 18/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
